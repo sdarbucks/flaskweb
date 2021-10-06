b70e06d712ea76194ae4b2d5c53aa1af5afe7088
sdarbucks <kdh797979@naver.com>
2021-10-06 10:05:02 +0900

    js work

--- example/app.py
+++ example/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 127.0.0.1
+# 127.0.0.1:5000
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,5 +17,11 @@ def loopindex():
 @app.route('/node')
 def node():
     return render_template('node.html')
+
+# 127.0.0.1:5000/nodeimg
+@app.route('/nodeimg')
+def nodeimg():
+    return render_template('nodeimg.html')
+
 
 app.run()
